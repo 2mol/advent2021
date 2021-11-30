a71508e469cf6408e6343182d2e0a380774995f6
2mol <2mol@users.noreply.github.com>
2021-11-30 21:37:07 +0100

    2-1

---
+++ Prev_Day02.fs
@@ -0,0 +1,51 @@
+module Prev_Day02
+
+open System
+open System.IO
+
+type LineSpec = {
+    Min: int
+    Max: int
+    Letter: Char
+    Password: string
+}
+
+let splitLine (line : string) =
+    let min, rest =
+        match line.Split "-" with
+        | [| min; rest |] -> min, rest
+        | _ -> failwith "fuck"
+    let max, charColon, password =
+        match rest.Split " " with
+        | [| max; charColon; password |] -> max, charColon, password
+        | _ -> failwith "fuck"
+    {
+        Min = (int)min
+        Max =  (int)max
+        Letter = charColon.Chars(0)
+        Password = password //|> Seq.toArray
+    }
+
+let isValid (lineSpec : LineSpec) : bool =
+    let charCount =
+        Seq.countBy id lineSpec.Password
+        |> Map.ofSeq
+        |> Map.tryFind lineSpec.Letter
+        |> Option.defaultValue 0
+
+    charCount >= lineSpec.Min && charCount <= lineSpec.Max
+
+let entries =
+    File.ReadLines "inputs/2020_day2.txt"
+    |> Seq.map splitLine
+    |> List.ofSeq
+
+let solution1 =
+    entries
+    |> List.filter isValid
+    |> List.length
+    |> string
+
+let solution2 =
+    ""
+    |> string
--- Program.fs
+++ Program.fs
@@ -2,6 +2,8 @@
     [
         "2020 day 1-1", Prev_Day01.solution1
         "2020 day 1-2", Prev_Day01.solution2
+        "2020 day 2-1", Prev_Day02.solution1
+        "2020 day 2-2", Prev_Day02.solution2
     ]
 
 for day, solution in solutions do
