508d2cd53adbb0513040fdce27e86746647b389a
2mol <2mol@users.noreply.github.com>
2021-11-30 20:57:43 +0100

    wip

--- Prev_Day01.fs
+++ Prev_Day01.fs
@@ -2,15 +2,19 @@ module Prev_Day01
 
 open System.IO
 
-let solution1 =
-    let entries =
-        File.ReadLines "inputs/2020_day1a.txt"
-        |> Seq.map int
-        |> Set.ofSeq
+let entries =
+    File.ReadLines "inputs/2020_day1.txt"
+    |> Seq.map int
+    |> Set.ofSeq
 
+let solution1 =
     entries
     |> Set.filter (fun el -> Set.contains (2020 - el) entries)
     |> Set.fold (*) 1
+    |> string
 
-
-let solution2 = 0
+let solution2 =
+    entries
+    |> Set.filter (fun el -> Set.contains (2020 - el) entries)
+    // |> Set.fold (*) 1
+    |> string
--- Program.fs
+++ Program.fs
@@ -1,8 +1,8 @@
-﻿let solutions =
+﻿let solutions : list<string * string> =
     [
         "2020 day 1-1", Prev_Day01.solution1
         "2020 day 1-2", Prev_Day01.solution2
     ]
 
 for day, solution in solutions do
-    printfn "%s: %A" day solution
+    printfn "%s: %s" day solution
