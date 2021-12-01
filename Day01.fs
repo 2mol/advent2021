44f3e4a45355b1a2fd2356cc553e3780d89f922a
2mol <2mol@users.noreply.github.com>
2021-12-01 09:44:59 +0100

    factor out because

--- Day01.fs
+++ Day01.fs
@@ -5,23 +5,22 @@ open System.IO
 let entries =
     File.ReadLines "inputs/day1.txt"
     |> Seq.map int
-    |> Seq.toList
+    |> Seq.toList // this is necessary to consume all the lines upfront!
+
+let largerThanPreviousCount list =
+    Seq.zip list (Seq.tail list)
+    |> Seq.map (fun (a, b) -> a < b)
+    |> Seq.filter id
+    |> Seq.length
 
 let solution1 =
-    List.zip entries[.. 1998] (List.tail entries)
-    |> List.map (fun (a, b) -> a < b)
-    |> List.filter id
-    |> List.length
+    largerThanPreviousCount entries
     |> string
 
-// let solution2 =
-//     seq {
-//         for el1 in entries do
-//         for el2 in entries do
-//         if Set.contains (2020 - el1 - el2) entries
-//             then yield el1, el2
-//     }
-//     |> Seq.toList
-//     |> List.head
-//     |> fun (a, b) -> a * b * (2020 - a - b)
-//     |> string
+let solution2 =
+    let depthSums =
+        Seq.windowed 3 entries
+        |> Seq.map Array.sum
+
+    largerThanPreviousCount depthSums
+    |> string
--- Program.fs
+++ Program.fs
@@ -1,6 +1,7 @@
 ﻿let solutions : list<string * string> =
     [
         "day 1-1", Day01.solution1
+        "day 1-2", Day01.solution2
     ]
 
 for day, solution in solutions do
