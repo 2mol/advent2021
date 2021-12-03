c87b0eefc58d8eb9dd48f9392bfa9b73cede12f6
2mol <2mol@users.noreply.github.com>
2021-12-03 15:13:26 +0100

    benchmarking experiments

--- Day03.fs
+++ Day03.fs
@@ -3,11 +3,14 @@ module Day03
 open System
 open System.IO
 
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
+
 #nowarn "25"
 
-let input =
-    File.ReadLines "inputs/input3.txt"
-    |> Seq.toList
+// let input =
+//     File.ReadLines "inputs/input3.txt"
+//     |> Seq.toList
 
 let MeasurementLength = 12
 
@@ -20,7 +23,7 @@ let aggregate (counter : int array) (str : string) =
             | _ -> count
         )
 
-let solution1 =
+let solution1 input =
     List.fold aggregate (Array.zeroCreate MeasurementLength) input
     |> Array.map (fun n -> n > List.length input / 2 )
     |> Array.map (fun b -> if b then "1" else "0")
@@ -61,8 +64,90 @@ let rec solutionHelper (pos : int) (criterion : BitCriterion) (measurements : st
 
         solutionHelper (pos + 1) criterion subMeasurements
 
-let solution2 =
+let solution2 input =
     [solutionHelper 0 OxygenGenerator input; solutionHelper 0 CO2Scrubber input]
     |> List.map (fun b -> Convert.ToInt32(b, 2))
     |> List.fold (*) 1
     |> sprintf "%A"
+
+// ----------------------------------------------------------
+
+let rec solutionHelperNoBitCriterion (pos : int) (desiredChar : char) (measurements : string seq) =
+    if Seq.length measurements = 1 then
+        Seq.head measurements
+    else
+        let discriminator =
+            match desiredChar with
+            | '1' -> Seq.maxBy
+            | '0' -> Seq.minBy
+
+        let counter = Seq.groupBy (fun (str : string) -> str[pos]) measurements
+
+        let equalOccurrences =
+            Seq.map (snd >> Seq.length) counter
+            |> Set.ofSeq
+            |> Set.count
+            |> (=) 1
+
+        let subMeasurements =
+            if equalOccurrences then
+                Map.ofSeq counter |> Map.find desiredChar
+            else
+                discriminator (snd >> Seq.length) counter |> snd
+
+        solutionHelperNoBitCriterion (pos + 1) desiredChar subMeasurements
+
+let solution2NoBitCriterion input =
+    [solutionHelperNoBitCriterion 0 '1' input; solutionHelperNoBitCriterion 0 '0' input]
+    |> List.map (fun b -> Convert.ToInt32(b, 2))
+    |> List.fold (*) 1
+    |> sprintf "%A"
+
+// ----------------------------------------------------------
+
+let rec solutionHelperOpt (pos : int) (desiredChar : char) (measurements : string seq) =
+    if Seq.length measurements = 1 then
+        Seq.head measurements
+    else
+        let counter = Seq.groupBy (fun (str : string) -> str[pos]) measurements
+
+        let equalOccurrences =
+            Seq.map (snd >> Seq.length) counter
+            |> Set.ofSeq
+            |> Set.count
+            |> (=) 1
+
+        match desiredChar, equalOccurrences with
+        | _, true ->
+            counter
+            |> Seq.filter (fst >> (=) desiredChar)
+            |> Seq.head
+        | '0', _ ->
+            Seq.minBy (snd >> Seq.length) counter
+        | '1', _ ->
+            Seq.maxBy (snd >> Seq.length) counter
+        |> snd
+        |> solutionHelperOpt (pos + 1) desiredChar
+
+let solution2Opt input =
+    [solutionHelperOpt 0 '1' input; solutionHelperOpt 0 '0' input]
+    |> List.map (fun b -> Convert.ToInt32(b, 2))
+    |> List.fold (*) 1
+    |> sprintf "%A"
+
+// ----------------------------------------------------------
+
+[<MemoryDiagnoser>]
+type Benchmark2 () =
+    member __.Input =
+        File.ReadLines "/Users/juri/code/advent2021/inputs/input3.txt"
+        |> Seq.toList
+
+    [<Benchmark(Baseline=true)>]
+    member self.Solution2Initial() = solution2 self.Input
+
+    [<Benchmark>]
+    member self.Solution2NoBitCriterion() = solution2NoBitCriterion self.Input
+
+    [<Benchmark>]
+    member self.Solution2Opt() = solution2Opt self.Input
--- Program.fs
+++ Program.fs
@@ -1,12 +1,21 @@
-﻿let solutions : list<string * string> =
-    [
-        // "day 1-1", Day01.solution1
-        // "day 1-2", Day01.solution2
-        // "day 2-1", Day02.solution1
-        // "day 2-2", Day02.solution2
-        "day 3-1", Day03.solution1
-        "day 3-2", Day03.solution2
-    ]
+﻿open BenchmarkDotNet.Running
 
-for day, solution in solutions do
-    printfn "%s: %s" day solution
+// let solutions : list<string * string> =
+//     [
+//         // "day 1-1", Day01.solution1
+//         // "day 1-2", Day01.solution2
+//         // "day 2-1", Day02.solution1
+//         // "day 2-2", Day02.solution2
+//         "day 3-1", Day03.solution1
+//         "day 3-2", Day03.solution2
+//     ]
+
+// for day, solution in solutions do
+//     printfn "%s: %s" day solution
+
+[<EntryPoint>]
+let main _ =
+    let summary = BenchmarkRunner.Run<Day03.Benchmark2>()
+    printfn "%A" summary
+
+    0 // return an integer exit code
