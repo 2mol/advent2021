54c8369f58fabd6241add2d2791b6759cf34ee73
2mol <2mol@users.noreply.github.com>
2021-12-10 12:59:06 +0100

    10-2

--- Day10.fsx
+++ Day10.fsx
@@ -50,4 +50,46 @@ input
 |> Array.choose (findTheSHitBracket [])
 |> Array.map (fun c -> Map.find c bracketPoints)
 |> Array.sum
-|> printfn "%A"
+|> printfn "day10-1: %i"
+
+
+let bracketAutocompletePoints =
+    [
+        ')', 1UL
+        ']', 2UL
+        '}', 3UL
+        '>', 4UL
+    ] |> Map.ofList
+
+let rec findTheClosers expectationStack incomingStack : char list =
+    match incomingStack with
+    | [] -> expectationStack // failwithf "nooo %A" expectationStack
+    | c::cs ->
+        if isOpeningBracket c then
+            let closer = Map.find c brackets
+            findTheClosers (closer::expectationStack) cs
+        else
+            // c is a closing bracket, it should be on top of the expectationStack
+            match expectationStack with
+            | [] ->
+                // we have a closing bracket, but didn't expect anything? => corrupted!
+                failwith "we should have filtered out corrupted lines"
+            | x::expect when c = x ->
+                // we see exactly the character that we were expecting? Nice, consume both and continue.
+                findTheClosers expect cs
+            | _ ->
+                // we have a closing bracket that mismatches the expected one? => corrupted.
+                failwith "we should have filtered out corrupted lines"
+
+let scoreLine chars =
+    Seq.map (fun c -> Map.find c bracketAutocompletePoints) chars
+    |> Seq.fold (fun accScore charScore -> accScore * 5UL + charScore) 0UL
+
+input
+|> Array.map List.ofSeq
+|> Array.filter (fun line -> Option.isNone <| findTheSHitBracket [] line)
+|> Array.map (findTheClosers [])
+|> Array.map scoreLine
+|> Array.sort
+|> fun a -> a[24]
+|> printfn "day10-2: %A"
