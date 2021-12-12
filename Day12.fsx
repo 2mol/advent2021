0ce081ec3e7f8cd5dabf909c12134674c6e9f1df
2mol <2mol@users.noreply.github.com>
2021-12-12 10:37:42 +0100

    12-2

--- Day12.fsx
+++ Day12.fsx
@@ -17,13 +17,59 @@ let input =
     System.IO.File.ReadLines "inputs/input12.txt"
     |> Seq.map parse
 
-let canTraverse currentPath (str:string) =
+let canTraverse1 currentPath (str:string) =
     if Char.IsUpper str[0] then true
     else not <| List.contains str currentPath
+
+let rec traverse1 currentPath map =
+    let currentNode = List.head currentPath
+    if currentNode = "end" then
+        [ List.rev currentPath ]
+    else
+        let nextNodes =
+            Map.tryFind currentNode map
+            |> Option.defaultValue []
+            |> List.filter (canTraverse1 currentPath)
+
+        [ for node in nextNodes do yield! traverse1 (node::currentPath) map ]
+
+// input
+let map1 = Seq.fold (fun m (k,v) -> Map.addToList k v m) Map.empty input
+let map = Seq.fold (fun m (k,v) -> Map.addToList k v m) map1 (Seq.map Tuple.flip input)
+
+map
+|> traverse1 ["start"]
+|> List.length
+|> printfn "day12-1: %i"
+
+// ----------------------------------------------------------------------------
+
+let canTraverse pathSoFar (nextNode:string) =
+    if Char.IsUpper nextNode[0] then
+        true
+    else
+        // let notVisitedYet = not <| List.contains str pathSoFar
+        let smallCavesVisitorCount =
+            pathSoFar
+            |> List.filter (fun (str:string) -> Char.IsLower str[0])
+            |> List.countBy id
+            |> Map.ofList
+
+        // printfn "%A" smallCavesVisitorCount
+
+        let canVisit =
+            (nextNode <> "start") && (
+                false
+                || Set.ofSeq (Map.values smallCavesVisitorCount) = Set.empty
+                || Set.ofSeq (Map.values smallCavesVisitorCount) = set [1]
+                || not (Map.containsKey nextNode smallCavesVisitorCount)
+            )
+        canVisit
 
 let rec traverse currentPath map =
     let currentNode = List.head currentPath
     if currentNode = "end" then
+        // printfn "%s" <| String.concat "," (List.rev currentPath)
         [ List.rev currentPath ]
     else
         let nextNodes =
@@ -33,11 +79,7 @@ let rec traverse currentPath map =
 
         [ for node in nextNodes do yield! traverse (node::currentPath) map ]
 
-// input
-let map1 = Seq.fold (fun m (k,v) -> Map.addToList k v m) Map.empty input
-let map = Seq.fold (fun m (k,v) -> Map.addToList k v m) map1 (Seq.map Tuple.flip input)
-
 map
 |> traverse ["start"]
 |> List.length
-|> printfn "%A"
+|> printfn "day12-2: %A"
