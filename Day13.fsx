49eced51037e9c41ec9a48533113225844cfa09d
2mol <2mol@users.noreply.github.com>
2021-12-13 10:47:46 +0100

    13-1 wip

--- Day13.fsx
+++ Day13.fsx
@@ -39,26 +39,45 @@ let grid = Array2D.create maxx maxy '.'
 for (x, y) in coords do
     grid[x,y] <- '#'
 
-// for i in [0..maxx-1] do
-//     for j in [0..maxy-1] do
-//         printf "%c" grid[i,j]
-//     printfn ""
+let foldAlongX xSplit (g : char[,]) =
+    let half1 = g[0..xSplit-1, *]
+    let half2 = g[xSplit+1.., *]
 
-let half1 = grid[0..654, *]
-let half2 = grid[656.., *]
+    Array2D.iteri (
+        fun i j v ->
+            if v = '#' then
+                half1[Array2D.length1 half1 - i - 1, j] <- v
+    ) half2
 
-// for i in [0..maxx-1] do
-//     for j in [0..maxy-1] do
-//         printf "%c" grid[i,j]
-//     printfn ""
+    half1
 
-Array2D.iteri (
-    fun i j v ->
-        if v = '#' then
-            half1[Array2D.length1 half1 - i, j] <- v
-) half2
+let foldAlongY ySplit (g : char[,]) =
+    let half1 = g[*, 0..ySplit-1]
+    let half2 = g[*, ySplit+1..]
 
-half1
+    Array2D.iteri (
+        fun i j v ->
+            if v = '#' then
+                half1[i, Array2D.length2 half1 - j - 1] <- v
+    ) half2
+
+    half1
+
+
+let printGrid (g : char[,]) =
+    for j in [0..Array2D.length2 g - 1] do
+        for i in [0..Array2D.length1 g - 1] do
+            printf "%c" g[i,j]
+        printfn ""
+
+let folded =
+    foldAlongY 7 grid
+    |> foldAlongX 5
+
+printGrid grid
+printGrid folded
+
+folded
 |> Array2D.flatten
 |> Array.sumBy (fun a -> if a = '#' then 1 else 0)
 |> printfn "%A"
