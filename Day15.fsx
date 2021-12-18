bbf6d63f0cbf4a2d72529340d025db7766a1c187
2mol <2mol@users.noreply.github.com>
2021-12-18 23:56:58 +0100

    wip perf wrong

--- Day15.fsx
+++ Day15.fsx
@@ -1,9 +1,11 @@
 #load "Extensions.fsx"
 open Extensions
 
-open System
-
 #nowarn "25"
+
+open System
+open System.Collections.Generic
+
 
 // live reload with:
 // -----------------
@@ -31,22 +33,19 @@ let getNeighbours x y =
     |> List.choose tryGet
     |> Set.ofList
 
-let graph : Map<Node, (Node*Cost) Set> =
+let graph : IDictionary<Node, (Node*Cost) Set> =
     [
         for x in 0..(Array2D.length1 input - 1) do
         for y in 0..(Array2D.length2 input - 1) do
         yield (x, y) , getNeighbours x y
     ]
-    |> Map.ofList
+    |> dict
 
 let goalNode = (Array2D.length1 input - 1, Array2D.length2 input - 1)
 
 let initDist : Map<Node, Cost> =
-    graph
-    // hacky bullshit to act as "infinity", but without overflowing
-    // in the `dist[bla] + 9` case
-    |> Map.mapValues (fun _ -> UInt32.MaxValue - 10u)
-    |> Map.add (0,0) 0u
+    ((0,0), 0u) :: [for kv in graph do yield (kv.Key, UInt32.MaxValue - 10u)]
+    |> Map.ofList
 
 let initQueue : Set<Node> = Map.keys initDist |> Set.ofSeq
 
@@ -64,7 +63,7 @@ let rec djikstra (queue : Set<Node>) (distances : Map<Node, Cost>) (fronteer : N
         Map.find u distances
     else
         let neighbours =
-            Map.find u graph
+            graph[u]
             |> Set.filter (fun (v, _) -> Set.contains v queue)
 
         let altDistances =
