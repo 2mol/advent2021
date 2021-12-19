d9efb361d50a69d658b535548c2a707240ffd6c0
2mol <2mol@users.noreply.github.com>
2021-12-19 17:22:54 +0100

    16-1 almost works

--- Day16.fsx
+++ Day16.fsx
@@ -13,7 +13,7 @@ open System
 printfn "---------- \u001b[31mlet's go!\u001b[0m ----------"
 
 let input =
-    System.IO.File.ReadLines "inputs/input16small.txt"
+    System.IO.File.ReadLines "inputs/input16.txt"
     |> Seq.head
     |> Seq.toArray
 
@@ -44,7 +44,6 @@ type Packet =
         Content : Content
     }
 and Content =
-    | Empty
     | Literal of int
     | Operator of Packet list
 
@@ -60,10 +59,9 @@ let rec extractLiteral ((str, acc) : string * (string list)) : string * string =
     else
         extractLiteral (str[5..], List.append acc [str[1..4]])
 
-let rec parse (str : string) =
-    printfn "parsing %s" str
+let rec parse (limit : int) (str : string) : (Packet list) * string =
     if String.length str < 6 || Set.ofSeq str = set ['0'] then
-        []
+        [], str
     else
         let version = str[0..2] |> bitsToInt
         let typeId = str[3..5] |> bitsToInt
@@ -71,38 +69,62 @@ let rec parse (str : string) =
         match typeId with
         | 4 ->
             // Literal packet
-            let literalBits, rest = extractLiteral (str[6..], [])
+            let literalBits, literalRest = extractLiteral (str[6..], [])
             let packet =
                 {
                     Version = version
                     TypeId = typeId
                     Content = Literal (bitsToInt literalBits)
                 }
-            packet :: parse rest
+            if limit = 1 then
+                [packet], literalRest
+            else if limit > 1 then
+                let packets, rest = parse (limit - 1) literalRest
+                packet :: packets, rest
+            else
+                let packets, rest = parse 0 literalRest
+                packet :: packets, rest
         | _ ->
             // Operator packet
             let lengthTypeId = str[6]
             let subPacketFrom =
                 if lengthTypeId = '0' then 15 else 11
                 |> (+) 7
-            let subPacketsLength =
+            let subPacketsInfo =
                 str[7..subPacketFrom-1]
                 |> bitsToInt
-            let subPacketsTo = subPacketFrom+subPacketsLength
-            let strSubPackets = str[subPacketFrom..subPacketsTo-1]
-            let rest = str[subPacketsTo..]
+            let subPackets, subRest =
+                if lengthTypeId = '0' then
+                    let subPacketsTo = subPacketFrom+subPacketsInfo
+                    let strSubPackets = str[subPacketFrom..subPacketsTo-1]
+                    let rest0 = str[subPacketsTo..]
+                    let packets1, rest1 = parse 0 strSubPackets // rest1 SHOULD be empty...
+                    packets1, rest0
+                else
+                    parse subPacketsInfo str[subPacketFrom..]
+
             let packet =
                 {
                     Version = version
                     TypeId = typeId
-                    Content = Operator (parse strSubPackets)
+                    Content = Operator subPackets
                 }
-            packet :: parse rest
+            let packets, rest = parse 0 subRest
+            packet :: packets, rest
+
+let rec sumVersionNumbers (packet : Packet) =
+    match packet.Content with
+    | Literal _ -> packet.Version
+    | Operator subPackets ->
+        packet.Version + (List.sumBy sumVersionNumbers subPackets)
+
 
 let inputBits =
     input
     |> Array.map toBin
     |> String.concat ""
 
-parse inputBits
+parse 0 inputBits
+|> fst
+|> List.sumBy sumVersionNumbers
 |> printfn "%A"
