3722c1fe68dcaec0e242eb03ac8e5bf859a37952
2mol <2mol@users.noreply.github.com>
2021-12-21 13:03:15 +0100

    wip

--- Day20.fsx
+++ Day20.fsx
@@ -26,36 +26,48 @@ let image =
     |> Array2D.map ((=) '#')
 
 
-// let draw a =
-//     [ for y in [ymin..ymax] do
-//         yield [
-//             for x in [xmin..xmax] do
-//             if x=0 && y=0 then
-//                 yield 'S'
-//             else if List.contains (x, y) shotTrail then
-//                 yield '#'
-//             else if x >= txmin && x <= txmax && y >= tymin && y <= tymax then
-//                 yield 'T'
-//             else
-//                 yield '.'
-//         ]
-//     ]
-//     |> List.map (fun cs -> String.Concat(cs))
-//     |> String.concat "\n"
+let draw a =
+    let l1 = Array2D.length1 a
+    let l2 = Array2D.length2 a
+    [
+        for j in [0..l2-1] do
+        yield [
+            for i in [0..l1-1] do
+            if a[i,j] then '#' else '.'
+        ]
+    ]
+    |> List.map (fun cs -> String.Concat(cs))
+    |> String.concat "\n"
 
-let extend a =
-    let l1 = Array2D.length1 a + 2
-    let l2 = Array2D.length2 a + 2
+let extend n a =
+    let l1 = Array2D.length1 a + 2*n
+    let l2 = Array2D.length2 a + 2*n
     Array2D.init l1 l2
         (fun i j ->
-            if i = 0 || j = 0 then
-                false
-            else if i = l1 || j = l2 then
+            if i < n || j < n || i > l1-1-n || j > l2-1-n then
                 false
             else
-                a[i-1,j-1]
+                a[i-n,j-n]
         )
+
+// let ENHANCE a =
+//     let l1 = Array2D.length1 a
+//     let l2 = Array2D.length2 a
+
+//     Array2D.init l1 l2
+//         (fun i j ->
+//             a[]
+//         )
+
+    // for i in [0..l1-1] do
+    //     for j in [0..l2-1] do
+    //         let
+    //         a[i,j] <- false
 
 
 image
-|> printfn "day20-1: %A"
+|> extend 2
+// |> ENHANCE
+|> draw
+|> printfn "%s"
+// |> printfn "day20-1: %A"
