98987cbe65c7d082a4c3508bfdad7f36734da66d
2mol <2mol@users.noreply.github.com>
2021-12-23 08:00:20 +0100

    18-1 wip

--- Day18.fsx
+++ Day18.fsx
@@ -3,7 +3,6 @@
 #nowarn "25"
 
 open System
-open FParsec
 open Extensions
 
 // live reload with:
@@ -16,33 +15,38 @@ let input =
     System.IO.File.ReadLines "inputs/input18sm.txt"
     |> Seq.toList
 
+// ----------------------- data type and parser ------------------------------
 
 type Tree =
     | Branch of Tree * Tree
     | Leaf of int
 
-let (tree : Parser<Tree, unit>), treeRef = createParserForwardedToRef()
-let leaf : Parser<Tree, unit> = pint32 |>> Leaf
-let branch = (tree <|> leaf) .>>. (skipChar ',' >>. (tree <|> leaf)) |>> Branch
-do treeRef.Value <- between (pchar '[') (pchar ']') branch
+module Parse =
+    open FParsec
+    let (tree : Parser<Tree, unit>), treeRef = createParserForwardedToRef()
+    let leaf : Parser<Tree, unit> = pint32 |>> Leaf
+    let branch = (tree <|> leaf) .>>. (skipChar ',' >>. (tree <|> leaf)) |>> Branch
+    do treeRef.Value <- between (pchar '[') (pchar ']') branch
+
+    let toOption presult =
+        match presult with
+        | Success (v, _, _) -> Some v
+        | Failure _ -> None
+
+    let parse = run tree >> toOption
 
 // run tree "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]"
 // |> printfn "%A"
 
-let parserToOption presult =
-    match presult with
-    | Success (v, _, _) -> Some v
-    | Failure _ -> None
 
 let inputNumbers =
     input
-    |> List.choose (run tree >> parserToOption)
+    |> List.choose Parse.parse
     // |> printfn "%A"
 
-type Direction = Left | Right
+// ------------------------ tree algorithms ----------------------------------
 
-let add t1 t2 =
-    Branch (t1, t2)
+type Direction = Left | Right
 
 let rec look t path =
     match path, t with
@@ -63,10 +67,16 @@ let rec replace mainT path subT =
         Branch (t1, newT2)
     | _ -> failwith "can't reach the spot"
 
-
 let rec findExploders t (path : Direction list) : ((Direction list)*(int*int)) list =
     match t with
     | Branch (Leaf a, Leaf b) -> []
     | _ -> [[],(0,0)]
 
+let findValue direction path : Direction list = []
+
+// --------------------------- addition rules etc -----------------------------
+
 let rec reduce t = t
+
+let add t1 t2 =
+    Branch (t1, t2)
